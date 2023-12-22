cb9eb33e5d6cb19dcc6b4cbe73501533e81e5d05
Mdmahmudkandoker <79156985+Mdmahmudkandoker@users.noreply.github.com>
2023-12-22 15:57:30 +0600

    updated some information

--- script.js
+++ script.js
@@ -31,13 +31,13 @@ $(document).ready(function(){
     });
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: [" Junior web Developer", "Javascript Developer", "Mern stack Developer"],
+        strings: ["Business IT specialist"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
     var typed = new Typed(".typing-2", {
-        strings: ["Front end web Developer", " Junior web Developer","Javascript Developer" , "Mern stack Developer" ],
+        strings: ["Business IT specialist"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
